8b1eaf3b4d8a56e1c0d7ae686ef3c3dfbc434d23
DianSano <alb.vds@gmail.com>
2019-07-22 11:21:37 +0700

    update 1 utk MainActivity.java

--- app/src/main/java/id/co/blogspot/diansano/appcopydbfromassets/MainActivity.java
+++ app/src/main/java/id/co/blogspot/diansano/appcopydbfromassets/MainActivity.java
@@ -1,7 +1,16 @@
 package id.co.blogspot.diansano.appcopydbfromassets;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            String destPath = "/data/data/" + getPackageName() + "/databases";
+            File f = new File(destPath);
+            if (!f.exists()) {
+                f.mkdirs();
+                f.createNewFile();
+                // --copy db dari folder asset ke folder database
+                copyDB(getBaseContext().getAssets().open("anggota.db"), new FileOutputStream(destPath + "/anggota.db"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DBAdapter db = new DBAdapter(this);
+        db.open();
+        Cursor c = db.getAllContacts();
+        if (c.moveToFirst()) {
+            do {
+                DisplayContact(c);
+            } while (c.moveToNext());
+        }
+        db.close();
+    }
+
+    public void DisplayContact(Cursor c) {
+        Toast.makeText(this, "id: " + c.getString(0) + "\n" + "Name: " +
+                c.getString(1) + "\n" + "Email: " + c.getString(2),
+        Toast.LENGTH_SHORT).show();
+    }
+
+    private void copyDB(InputStream inputStream, OutputStream outputStream) throws IOException {
+        //--copy 1K bytes at a time
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+        inputStream.close();
+        outputStream.close();
     }
 }
